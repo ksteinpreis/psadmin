0fba0d0ddc241e3cd679d38da29558429adc4bf7
Kurt Steinpreis <kurt.steinpreis@gmail.com>
2015-10-10 18:41:51 -0500

    additional author form mods

--- src/components/about/aboutPage.js
+++ src/components/about/aboutPage.js
@@ -3,6 +3,21 @@
 var React = require('react');
 
 var About = React.createClass({
+	statics: {
+		willTransitionTo: function(transistion, params, query, callback) {
+			if(!confirm('Are you sure you want to read a page that\'s this boring?')) {
+				transistion.abort();
+			} else {
+				callback();
+			}
+		},
+		willTransitionFrom: function(transistion, component) {
+			if(!confirm('Are you sure you want to leave a page this exciting?')) {
+				transistion.abort();
+			}
+		}
+	},
+
 	render: function() {
 		return (
 			<div>
